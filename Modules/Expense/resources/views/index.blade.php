56ecfa857486f7e88780f0811bd0ff0094306e18
Ilija <ilijaristov67@gmail.com>
2025-07-26 12:46:45 +0200

    deleted unecesarry model, created admin module

--- Modules/Expense/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-expense::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('expense.name') !!}</p>
-</x-expense::layouts.master>
--- Modules/Expense/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Expense\Http\Controllers\ExpenseController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('expenses', ExpenseController::class)->names('expense');
-});
