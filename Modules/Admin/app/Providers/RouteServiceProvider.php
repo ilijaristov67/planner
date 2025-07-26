56d1aec65f0b4339f5d8852dfdc2ba2203bb2bc3
Ilija <ilijaristov67@gmail.com>
2025-07-26 12:47:46 +0200

    created admin module

---
+++ Modules/Admin/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\Admin\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    protected string $name = 'Admin';
+
+    public function boot(): void
+    {
+        parent::boot();
+    }
+
+    public function map(): void
+    {
+        $this->mapApiRoutes();
+        $this->mapWebRoutes();
+    }
+
+    protected function mapWebRoutes(): void
+    {
+        Route::middleware('web')->group(module_path($this->name, '/routes/web.php'));
+    }
+
+    protected function mapApiRoutes(): void
+    {
+        Route::middleware('api')
+            ->prefix('api/admin')->name('api.admin.')
+            ->group(module_path($this->name, '/routes/api.php'));
+    }
+}
---
+++ Modules/Admin/database/seeders/AdminDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Admin\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Modules\Admin\Database\Seeders\PermissionSeeder\PermissionSeeder;
+
+class AdminDatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+         $this->call([
+             PermissionSeeder::class,
+         ]);
+    }
+}
